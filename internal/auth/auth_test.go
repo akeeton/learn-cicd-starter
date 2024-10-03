cd19c6e8f4382ad54799d5aa7d7b19989489978e
akeeton <andrewrkeeton@gmail.com>
2024-10-03 15:50:43 -0400

    Add auth_test.go and run tests in CI

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type args struct {
+		headers http.Header
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "Empty headers",
+			args:    args{headers: http.Header{}},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name:    "Good header",
+			args:    args{headers: http.Header{
+				"Authorization": []string{ "ApiKey ASDF" },
+			}},
+			want:    "ASDF",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.args.headers)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetAPIKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
